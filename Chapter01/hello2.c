69626e6ffa9bede269786da2cac4b855b04d9014
Jose Palmer <j_palmeros@yahoo.com.mx>
2021-12-20 05:44:26 -0600

    -m
    Terminado el Capítulo 1

---
+++ Chapter01/hello2.c
@@ -0,0 +1,20 @@
+/*
+ * hello2.c
+ * My first C program with comments
+ * by <Jose Palmer>
+ * created 20/07/2020
+ *
+ * compile with
+ *
+ * cc hello2.c -o hello2
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+
+/* eof */
